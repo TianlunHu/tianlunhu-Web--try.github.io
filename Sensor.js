9a1bee45ebfab86dcf3eb7eea1f2825dd16e0849
TianlunHu <39169925+TianlunHu@users.noreply.github.com>
2019-07-01 10:23:50 +0200

    Add files via upload

--- Sensor.js
+++ Sensor.js
@@ -1,93 +1,80 @@
 'use strict';
-//----------------- Motion Sensors -------------- //
-var TimeStamp = new Array();
-var AccVec = new Array();
-var rotVec = [];
-
-function accelerationHandler(acceleration, targetId) {
-  var info, xyz = "[X, Y, Z]";
-  info = xyz.replace("X", acceleration.x && acceleration.x.toFixed(3));
-  info = info.replace("Y", acceleration.y && acceleration.y.toFixed(3));
-  info = info.replace("Z", acceleration.z && acceleration.z.toFixed(3));
-  document.getElementById(targetId).innerHTML = info;
+if ('DeviceOrientationEvent' in window) {
+  window.addEventListener('deviceorientation', deviceOrientationHandler, false);
+} else {
+  document.getElementById('logoContainer').innerText = 'Device Orientation API not supported.';
 }
 
-function test(){
-    console.log("I'm executed");
-}
-
-function rotationHandler(rotation, RV, t) {
-  var info, xyz = "[t, X, Y, Z]";
-  info = xyz.replace("t", t);
-  info = info.replace("X", rotation.alpha && rotation.alpha.toFixed(3));
-  info = info.replace("Y", rotation.beta && rotation.beta.toFixed(3));
-  info = info.replace("Z", rotation.gamma && rotation.gamma.toFixed(3));
-  document.getElementById("moRotation").innerHTML = info;
-  document.getElementById("timeStamp").innerHTML = t;
-  RV.push(info);
-  document.getElementById('RotSequence').innerHTML = RV;
+function deviceOrientationHandler (eventData) {
+  var tiltLR = eventData.gamma;
+  var tiltFB = eventData.beta;
+  var dir = eventData.alpha;
   
+  document.getElementById("doTiltLR").innerHTML = Math.round(tiltLR);
+  document.getElementById("doTiltFB").innerHTML = Math.round(tiltFB);
+  document.getElementById("doDirection").innerHTML = Math.round(dir);
+
+  var logo = document.getElementById("imgLogo");
+  logo.style.webkitTransform = "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB * -1) + "deg)";
+  logo.style.MozTransform = "rotate(" + tiltLR + "deg)";
+  logo.style.transform = "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB * -1) + "deg)";
+}
+
+//----------------- Motion Sensors -------------- //
+function accelerationHandler(acceleration, targetId) {
+    var info, xyz = "[X, Y, Z]";
+
+    info = xyz.replace("X", acceleration.x && acceleration.x.toFixed(3));
+    info = info.replace("Y", acceleration.y && acceleration.y.toFixed(3));
+    info = info.replace("Z", acceleration.z && acceleration.z.toFixed(3));
+    document.getElementById(targetId).innerHTML = info;
+}
+
+function rotationHandler(rotation) {
+    var info, xyz = "[X, Y, Z]";
+
+    info = xyz.replace("X", rotation.alpha && rotation.alpha.toFixed(3));
+    info = info.replace("Y", rotation.beta && rotation.beta.toFixed(3));
+    info = info.replace("Z", rotation.gamma && rotation.gamma.toFixed(3));
+    document.getElementById("moRotation").innerHTML = info;
 }
 
 function intervalHandler(interval) {
-  document.getElementById("moInterval").innerHTML = interval;
+    document.getElementById("moInterval").innerHTML = interval;
 }
 
 if ('LinearAccelerationSensor' in window && 'Gyroscope' in window) {
-    document.getElementById('moApi').innerHTML = 'Motion Sensor detected';
+    document.getElementById('moApi').innerHTML = 'Motion Sensor';
+
     let lastReadingTimestamp;
-    let accelerometer = new LinearAccelerationSensor({frequency: 30});
-    
+    let accelerometer = new LinearAccelerationSensor();
     accelerometer.addEventListener('reading', e => {
-    if (lastReadingTimestamp) {
-        intervalHandler(Math.round(accelerometer.timestamp - lastReadingTimestamp));
-    }
-    lastReadingTimestamp = accelerometer.timestamp;
-    accelerationHandler(accelerometer, 'moAccel');
-    xyz[0] = accelerometer.x.toFixed(3);
-    xyz[1] = accelerometer.y.toFixed(3);
-    xyz[2] = accelerometer.z.toFixed(3);
-    AccVec.concat(xyz);
-    TimeStamp.concat(accelerometer.timestamp);
-    console.log("Acceleration along the X-axis " + accelerometer.x);
-    console.log("Acceleration along the Y-axis " + accelerometer.y);
-    console.log("Acceleration along the Z-axis " + accelerometer.z);
-    
-    console.log("here we are: " + TimeStamp + " >> "+ AccVec);
-    test();
+        if (lastReadingTimestamp) {
+            intervalHandler(Math.round(accelerometer.timestamp - lastReadingTimestamp));
+        }
+        lastReadingTimestamp = accelerometer.timestamp
+        accelerationHandler(accelerometer, 'moAccel');
     });
-    //test();
-    /*if (lastReadingTimestamp) {
-        intervalHandler(Math.round(accelerometer.timestamp - lastReadingTimestamp));
-    }
-    else{
-        lastReadingTimestamp = accelerometer.timestamp;
-    }
-    accelerationHandler(accelerometer, 'moAccel');*/
-    
     accelerometer.start();
 
     if ('GravitySensor' in window) {
         let gravity = new GravitySensor();
         gravity.addEventListener('reading', e => accelerationHandler(gravity, 'moAccelGrav'));
-        //accelerationHandler(gravity, 'moAccelGrav');
         gravity.start();
     }
 
     let gyroscope = new Gyroscope();
     gyroscope.addEventListener('reading', e => rotationHandler({
-    alpha: gyroscope.x,
-    beta: gyroscope.y,
-    gamma: gyroscope.z
-    },rotVec,Date.now()/1000));
-    //gyroscope.addEventListener('reading', e => rotVec.push())
+        alpha: gyroscope.x,
+        beta: gyroscope.y,
+        gamma: gyroscope.z
+    }));
     gyroscope.start();
-}
 
-else if ('DeviceMotionEvent' in window) {
-    document.getElementById('moApi').innerHTML = 'Device Motion Event';
+} else if ('DeviceMotionEvent' in window) {
+    document.getElementById('moApi').innerHTML = 'Device Motion API';
 
-    var onDeviceMotion = function(eventData) {
+    var onDeviceMotion = function (eventData) {
         accelerationHandler(eventData.acceleration, 'moAccel');
         accelerationHandler(eventData.accelerationIncludingGravity, 'moAccelGrav');
         rotationHandler(eventData.rotationRate);
@@ -95,9 +82,6 @@ else if ('DeviceMotionEvent' in window) {
     }
 
     window.addEventListener('devicemotion', onDeviceMotion, false);
-}
-
-else {
-    document.getElementById('moApi').innerHTML = 'No Sensors API available';
-    document.getElementById("moRotation").innerHTML = '[x,y,z]';
+} else {
+    document.getElementById('moApi').innerHTML = 'No Accelerometer & Gyroscope API available';
 }
